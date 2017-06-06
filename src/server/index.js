d2eb127d1aec5bdbbbfe7ac6505dca63137522c9
Ale Figueroa <alejandro@progressly.com>
2017-06-06 00:48:44 -0500

    Add Morgan and attach to logger

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,18 @@
 /* eslint import/no-unresolved: [2, { ignore: ['^~/'] }]*/
 import express from 'express';
+import morgan from 'morgan';
 import logger from '~/logger'; // eslint-disable-line import/extensions
 
 const { info } = logger();
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.use(
+  morgan('dev', {
+    buffer: false,
+    stream: { write: input => info(input.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
