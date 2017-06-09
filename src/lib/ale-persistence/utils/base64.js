7523bb5ada3815f0bfcfc53cda4c93fc9ad3685a
Ale Figueroa <alejandro@progressly.com>
2017-06-09 13:16:10 -0500

    Utils for cursor generation

---
+++ src/lib/ale-persistence/utils/base64.js
@@ -0,0 +1,28 @@
+/* @flow */
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+import stringify from 'json-stringify-safe';
+
+export type Base64String = string;
+
+export function base64(i: string): Base64String {
+  return new Buffer(i, 'utf8').toString('base64');
+}
+
+export function unbase64(i: Base64String): string {
+  return new Buffer(i, 'base64').toString('utf8');
+}
+
+export function stringify64(i: any): Base64String {
+  return base64(stringify(i));
+}
+
+export function parse64(i: Base64String): any {
+  return JSON.parse(unbase64(i));
+}
